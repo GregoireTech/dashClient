de8b5f90d01bdc716dda38038c0c29ba1835229b
Gregoire888 <greg.claise@gmail.com>
2019-05-18 13:56:32 +0200

    After taking out server & script from Git repo

--- client/src/assets/instances.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const instance = (token) => {
-    return (axios.create({baseURL: `https://dashserver.herokuapp.com/data?token=${token}`}));
-};
-
-export default instance;
--- client/src/assets/login.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const instance = (username, password) => {
-    return (axios.create({baseURL: `https://dashserver.herokuapp.com/login?user=${username}&pass=${password}`}));
-};
-
-export default instance;
