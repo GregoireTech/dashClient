a4de839d811b85964419666b2ddba2f1c1894235
Gregoire888 <greg.claise@gmail.com>
2019-05-19 18:06:01 +0200

    with Lead objective display & lead % kpi calculated in front end app

--- src/components/leadLeft/leadLeft.jsx
+++ src/components/leadLeft/leadLeft.jsx
@@ -7,14 +7,18 @@ import LeadBox from '../leadBox/leadBox';
 import Chart from '../chart/chart';
 
 const dashLeft = (props) => {
+    const salesGrowth = (props.monthData.Sales - props.monthData.Objective) / props.monthData.Objective;
+    const foodCost = props.monthData.FoodCost / props.monthData.Sales * 100;
+    const laborCost = props.monthData.LaborCost / props.monthData.Sales * 100;
     return (
         <div className='leadLeft'>
             <LeadBox title='Overall Month overview'>
+                <ScoreCard name='Sales Y-1' unit='$' score={props.monthData.Objective}/>
                 <ScoreCard name='Sales' unit='$' score={props.monthData.Sales}/>
-                <ScoreCard name='Growth' unit='%' score={props.monthData.Growth} down={0}/>
+                <ScoreCard name='Growth' unit='%' score={salesGrowth} down={0}/>
                 <ScoreCard name='Catering' unit='$' score={props.monthData.Catering}/>
-                <ScoreCard name='Food Cost' unit='%' score={props.monthData.FoodCostP} up={0.23} down={0.21}/>
-                <ScoreCard name='Labor Cost' unit='%' score={props.monthData.LaborCostP}/>
+                <ScoreCard name='Food Cost' unit='%' score={foodCost} up={0.23} down={0.21}/>
+                <ScoreCard name='Labor Cost' unit='%' score={laborCost}/>
             </LeadBox>
             <Chart
                 title='Overall Sales Graph ($)'
